dce600289dac79f0e35449426ddbc09c0d080a45
Harshit Mishra <harshitmishra2011@gmail.com>
2023-02-12 00:57:34 +0530

    changed api calls url

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -2,27 +2,27 @@ import axios from "axios";
 
 async function showall(queries,sort){ 
     const value = queries.value.toString()
-    const result = await axios.get(`http://localhost:3000/api/home/showAll?brand=${queries.brand}&category=${queries.category}&color=${queries.color}&search=${queries.search}&shipping=${queries.shipping}&value=${value}&sort=${sort}`)
+    const result = await axios.get(`https://shopping-backend-njuz.onrender.com/api/home/showAll?brand=${queries.brand}&category=${queries.category}&color=${queries.color}&search=${queries.search}&shipping=${queries.shipping}&value=${value}&sort=${sort}`)
     return result
 }
 
 async function listCategories(){
-    const result = await axios.get("http://localhost:3000/api/home/getCategory")
+    const result = await axios.get("https://shopping-backend-njuz.onrender.com/api/home/getCategory")
     return result
 }
 
 async function listBrands(){
-    const result = await axios.get("http://localhost:3000/api/home/getBrand")
+    const result = await axios.get("https://shopping-backend-njuz.onrender.com/api/home/getBrand")
     return result
 }
 
 async function listColor(){
-    const result = await axios.get("http://localhost:3000/api/home/getColor")
+    const result = await axios.get("https://shopping-backend-njuz.onrender.com/api/home/getColor")
     return result
 }
 
 async function details(id){
-    const result = await axios.get(`http://localhost:3000/api/home/details/${id}`)
+    const result = await axios.get(`https://shopping-backend-njuz.onrender.com/api/home/details/${id}`)
     return result
 }
 
